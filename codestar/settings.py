9cc32c9cb34acf8e066958666dd5c4a67c650126
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-04 11:50:59 +0000

    post model created

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,9 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+# check if secret key is set
+if not SECRET_KEY:
+    raise ValueError("The SECRET_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','https://github.com/NickOcaz/django-blog','.herokuapp.com']
 
