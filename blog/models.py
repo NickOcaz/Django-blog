fc44c2ce87637c83571f6726fe35ded1de8ae99d
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-04 22:53:38 +0000

    add migrations

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,5 @@
-# blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
-
-STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -11,19 +8,29 @@ class Post(models.Model):
     content = models.TextField()
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=((0, "Draft"), (1, "Published")), default=0)
     updated_on = models.DateTimeField(auto_now=True)
+    field_1 = models.CharField()
 
+    class Meta:
+        ordering = ["-created_on","author"]
+        
     def __str__(self):
-        return self.title
+        # return self.title
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=80)  # Ensure this line is added
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.name} on {self.post}"
+        return f"Comment by {self.author} on {self.post}"
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.author} on {self.post}"      
