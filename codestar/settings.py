7399dc725b9da00d3f0bace862cbcd3a98ca893a
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-04 11:33:36 +0000

    post model created

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*','.herokuapp.com']
+ALLOWED_HOSTS = ['*','https://github.com/NickOcaz/django-blog','.herokuapp.com']
 
 
 # Application definition
@@ -97,6 +97,12 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://github.com/NickOcaz/django-blog",
+    "https://*.herokuapp.com"
+]
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
