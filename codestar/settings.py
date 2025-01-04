68cd99d6ef15d64b558544ac65534302557c91f4
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-04 12:08:33 +0000

    post model created

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,15 +90,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 
 # db fix for heroku
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-# kpi suggestion
-# DATABASES = {
-#    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
