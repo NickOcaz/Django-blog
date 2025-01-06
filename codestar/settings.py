d7260b576846714c0c66ce706925da95976abbf9
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-06 12:03:58 +0000

    fix?

--- codestar/settings.py
+++ codestar/settings.py
@@ -34,7 +34,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','https://github.com/NickOcaz/django-blog','.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
