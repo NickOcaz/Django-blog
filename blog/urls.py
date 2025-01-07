336f2783abc2e0218535be41e42d70b7d52cba36
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-07 09:32:08 +0000

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='post_list'),  # Route for the homepage
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
 #urlpatterns = [
