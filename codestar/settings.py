2531927c54d77bda3b6db7c0469525d8d9095929
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-07 17:51:13 +0000

    Custom templates

--- codestar/settings.py
+++ codestar/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
     raise ValueError("The SECRET_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
