04793b12bd66330f017152d59d88214f55004d5b
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-08 16:00:52 +0000

    Challenge: Create the Collaboration Request form

--- about/models.py
+++ about/models.py
@@ -1,5 +1,5 @@
+# about/models.py
 from django.db import models
-
 
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +8,12 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- about/views.py
+++ about/views.py
@@ -1,15 +1,16 @@
+# about/views.py
 from django.shortcuts import render
 from .models import About
-
+from .forms import CollaborateForm
 
 def about_me(request):
     """
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
-
+    form = CollaborateForm()
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, "form": form},
     )
